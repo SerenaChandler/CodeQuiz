6318555f6d4b05b54e43d2666a211fefdcf9cd89
Serena Chandler <serenachandler2019@gmail.com>
2021-03-27 13:47:54 -0700

    pseuod code created

---
+++ script.js
@@ -0,0 +1,16 @@
+var startGame = document.querySelector("#start")
+var question = document.querySelector("#question")
+var answers = document.querySelector("#answers")
+
+var questionArray = []
+
+
+function renderTodos()
+    var test = document.createElement("test");
+    test.textContent = "test1";
+
+    li.appendchild(test)
+    question.appendChild(li)
+
+
+renderTodos()
