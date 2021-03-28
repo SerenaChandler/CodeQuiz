c3826c46c2b1ff2506f6ccb0ec5167d969dfa7df
Serena Chandler <serenachandler2019@gmail.com>
2021-03-28 16:08:13 -0700

    created a library contaning the objects for questions and answers

--- script.js
+++ script.js
@@ -1,16 +1,77 @@
-var startGame = document.querySelector("#start")
+var startButton = document.querySelector("#start")
 var question = document.querySelector("#question")
 var answers = document.querySelector("#answers")
-
-var questionArray = []
-
-
-function renderTodos()
-    var test = document.createElement("test");
-    test.textContent = "test1";
-
-    li.appendchild(test)
-    question.appendChild(li)
+var timerAmount = document.querySelector(".timerCounter")
 
 
-renderTodos()
+
+var chosenQuestion;
+var score = 0;
+var timer;
+var timerCounter;
+var myQuestions = [
+  {
+    question: "which language is not used to build website?",
+    answers: {
+      a: "HTML",
+      b: "Python",
+      c: "CSS"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "what does CSS stand for?",
+    answers: {
+      a: "Cascading Style Sheets",
+      b: "Child Super Star",
+      c: "Creative System Supplier"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "What is Javascript used for?",
+    answers: {
+      a: "Make a nice pot of coffee",
+      b: "add interactible elements to a page",
+      c: "Short for javaScripture, the digital bible",
+    
+    },
+    correctAnswer: "b"
+  }
+];
+
+
+function startQuiz() {
+  startButton.disabled = true;
+  
+  timerCounter = 60;
+  startTimer()
+  
+  
+  
+}
+
+
+function startTimer() {
+  timer = setInterval(function() {
+    
+    timerCounter--;
+
+    timerAmount.textContent = timerCounter;
+    
+    if (timerCounter <= 0) {
+      clearInterval(timer)
+    }
+      
+ }, 1000);
+}
+
+  function renderQuestions() {
+    
+    answers.textContent = myQuestions }
+  
+
+
+startButton.addEventListener("click", startQuiz)
+renderQuestions()
+
